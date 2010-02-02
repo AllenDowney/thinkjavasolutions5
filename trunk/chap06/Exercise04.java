27012f5be55db25e901b46b64e1271e515d98057
allendowney <allendowney@abf5b2da-0d1a-11df-9821-6b6fb8eff287>
2010-02-02 20:47:20 +0000

    Started the solution to Ex 6.4

---
+++ trunk/chap06/Exercise04.java
@@ -0,0 +1,19 @@
+package com.greenteapress.thinkapjava.chap06;
+
+/*
+ *
+ * Let's put the HTML for the question here.
+ *
+ * TODO(downey): generate the HTML version of the book!
+ * 
+ */
+
+public class Exercise04 {
+
+  public static float power(float x, int n) {
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+}
