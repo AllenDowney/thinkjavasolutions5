95d11df1df25c43c7e1d9f71dde4ddc6c98ce6ed
psingal18 <psingal18@abf5b2da-0d1a-11df-9821-6b6fb8eff287>
2010-08-13 00:07:44 +0000

    Exercise 10.7 spelling error change...

--- trunk/chap06/Exercise07.java
+++ trunk/chap06/Exercise07.java
@@ -5,7 +5,7 @@
 /* 
  * Exercise 6.7
  * 
- * One way to evaluate e^(-x^2) is to use the infinite serise expansion
+ * One way to evaluate e^(-x^2) is to use the infinite series expansion
  *     e^(-x^2) = 1 - 2x + 3x^2/2! - 4x^3/3! + 5x^4/4! - ...
  *     
  * In other words, we need to add up a series of terms where the ith term
